f7e2c88c2a3610830e167a26f6272a022ef643c9
rubabanat <rubabanat1996@gmail.com>
2021-06-25 11:39:45 +0300

    try to add products to the cart without connect the backend server yet

--- client/src/components/product/product.js
+++ client/src/components/product/product.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-function Product() {
-	return <div>Product</div>;
+function Product({ product, addToCart }) {
+	return (
+		<div>
+			<div>
+				<img src={product.image} alt="product image" />
+			</div>
+			<h1>{product.title}</h1>
+			<p>{product.description}</p>
+			<p>{product.price}</p>
+			<button onClick={() => addToCart(product)}>Add to my Cart</button>
+		</div>
+	);
 }
 
 export default Product;
--- client/src/components/products/products.js
+++ client/src/components/products/products.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Product from './../product/product';
+
+const productsArr = [
+	{
+		id: 1,
+		title: 'anything',
+		description: 'anything',
+		price: '100000000$',
+		image:
+			'',
+	},
+	{
+		id: 2,
+		title: 'anything',
+		description: 'anything',
+		price: '100000000$',
+		image:
+			'',
+	},
+	{
+		id: 3,
+		title: 'anything',
+		description: 'anything',
+		price: '100000000$',
+		image:
+			'',
+	},
+	{
+		id: 4,
+		title: 'anything',
+		description: 'anything',
+		price: '100000000$',
+		image:
+			'',
+	},
+];
 
 function Products() {
-	return <div>Products</div>;
+	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useState([]);
+
+	useEffect(() => {
+		setProducts([...productsArr, ...products]);
+	}, []);
+
+	const addToCart = (pro) => {
+		const product = cart.find((cartPro) => pro.id === cartPro.id);
+
+		if (!product) setCart([...cart, pro]);
+	};
+
+	return (
+		<>
+			{products.length ? (
+				<div>
+					{products.map((product) => {
+						return (
+							<Product
+								key={product.id}
+								product={product}
+								addToCart={addToCart}
+							/>
+						);
+					})}
+				</div>
+			) : (
+				<h1>There are no products</h1>
+			)}
+		</>
+	);
 }
 
 export default Products;
